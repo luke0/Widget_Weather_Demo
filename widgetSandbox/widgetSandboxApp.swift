38085e2b7b86cb5f6784e04ff8dfc0942e4c6f52
Luke Inger <luke.inger@jcb-finance.co.uk>
2020-07-16 13:47:03 +0100

    Initial Commit

---
+++ widgetSandbox/widgetSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  widgetSandboxApp.swift
+//  widgetSandbox
+//
+//  Created by Luke on 16/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct widgetSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
